948b5da94d6a84c8e91ca38a2fef6753bfb3ea70
nikkivno <nikkivigneault@gmail.com>
2023-09-16 09:36:25 -0300

    initial code added for Navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <div>
+            <div>About Me</div>
+            <div>Portfolio</div>
+            <div>Contact</div>
+        </div>
+    );
+}
+
+export default Navbar;
