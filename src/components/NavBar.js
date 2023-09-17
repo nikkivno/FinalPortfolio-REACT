5f9ae2f9d28bdb53fb5d39ac9c0dd659212aa2c1
nikkivno <nikkivigneault@gmail.com>
2023-09-17 10:14:47 -0300

    changed format of div in navbar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,11 +3,13 @@ import '../styles/Navbar.css'
 
 function Navbar() {
     return (
-        <div>
-            <div>About Me</div>
-            <div>Portfolio</div>
-            <div>Contact</div>
-        </div>
+    <nav>
+      <ul>
+        <li><a href="#aboutme">About Me</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
     );
 }
 
