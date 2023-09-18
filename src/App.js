942eefb3326064743f2766551702a6ba0388d2aa
nikkivno <nikkivigneault@gmail.com>
2023-09-18 13:40:51 -0300

    added in routing for each website section

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 
 function App() {
+    let Component 
+    switch (window.location.pathname) {
+        // case "/":
+        //     Component = App 
+        //     break 
+            case "/":
+                Component = AboutMe
+                break
+                case "/portfolio":
+                    Component = Portfolio
+                    break
+                    case "/contact":
+                        Component = Contact
+                        break
+    }
     return (
         <div>
             <Navbar />   
             <Header />
-            <AboutMe />    
-            <Contact />
+            <Component />
         </div>
     );
 }
