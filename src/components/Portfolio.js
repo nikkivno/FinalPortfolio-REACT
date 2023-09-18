e29e5650110787f15f9d2d413b77b0c9c0ad497b
nikkivno <nikkivigneault@gmail.com>
2023-09-18 12:17:25 -0300

    transfered portfolio code in, not corrected yet

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,7 +3,27 @@ import '../styles/Portfolio.css';
 
 function Portfolio() {
     return (
-
+        <article class="flexboxcontainer2" id="portfolio">
+        <div class="flexboxcontainer4">
+          <h2>My Work</h2>
+        </div>
+        <div class="flexboxcontainer6">
+          <div id="budget-website">
+            <a href="https://nikkivno.github.io/BudgetingWebsite-BootcampProject1/"><img id="budget-website-image" src="Assets/Images/budgetexample.png" alt="Link to BudgetSense Website"></a>
+          <div class="centered"> BudgetSense Webpage</div>
+        </div>  
+        <div id="mapbrew-website">
+          <a href="https://mapbrew-websitee-20a575919a4b.herokuapp.com/login"><img id="mapbrew-website-image" src="Assets/Images/mapbrewexample.png" alt="Link to Mapbrew Webpage"></a>
+        <div class="centered">Mapbrew Webpage</div>
+        </div>
+        <div id="horiseon-website">
+          <a href="https://nikkivno.github.io/BootcampChallenge1/#social-media-marketing"><img id="horiseon-website-image" src="Assets/Images/websiteexample.png" alt="Link to Horiseon Webpage"></a>
+          <div class="centered">Horiseon Webpage</div>
+          </div>
+          </div>  
+        </div>
+        </div>     
+    </article>
     );
 }
 
