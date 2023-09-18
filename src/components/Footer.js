b6345db2d6dbe05f04a374f367da531d45211b4f
nikkivno <nikkivigneault@gmail.com>
2023-09-18 19:46:00 -0300

    added Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/Footer.css'
+
+
+function Footer() {
+    return (
+        <footer>
+            <p>This website is property of Nikki Vigneault</p>
+        </footer>
+    );
+}
+
+export default Footer; 
+
+// FontAwesome Icons
+// Gitlab
+{/* <FontAwesomeIcon icon={faSquareGitlab} size="sm" style={{color: "#36454f",}} /> */}
+
+// LinkedIn
+{/* <FontAwesomeIcon icon={faLinkedin} size="sm" style={{color: "#35454f",}} /> */}
