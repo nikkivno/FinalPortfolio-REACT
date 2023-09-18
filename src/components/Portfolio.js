c3e68cb51e0c5bf3a65ad7acf0bef75b09a4a78c
nikkivno <nikkivigneault@gmail.com>
2023-09-18 14:48:02 -0300

    fixed image and div issues in portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import '../styles/Portfolio.css';
+import mapbrew from '../images/mapbrewexample.png';
+import budgetsense from '../images/budgetexample.png';
+import horiseon from '../images/websiteexample.png';
 
 function Portfolio() {
     return (
-        <article class="flexboxcontainer2" id="portfolio">
-        <div class="flexboxcontainer4">
+        <article className="flexboxcontainer2" id="portfolio">
+        <div className="flexboxcontainer4">
           <h2>My Work</h2>
         </div>
-        {/* <div class="flexboxcontainer6">
+        <div className="flexboxcontainer6">
           <div id="budget-website">
-            <a href="https://nikkivno.github.io/BudgetingWebsite-BootcampProject1/"><img id="budget-website-image" src="Assets/Images/budgetexample.png" alt="Link to BudgetSense Website"></a>
-          <div class="centered"> BudgetSense Webpage</div>
+            <a href="https://nikkivno.github.io/BudgetingWebsite-BootcampProject1/"><img id="budget-website-image" src={budgetsense} alt="Link to BudgetSense Website"/></a>
+          <div className="centered"> BudgetSense Webpage</div>
         </div>  
         <div id="mapbrew-website">
-          <a href="https://mapbrew-websitee-20a575919a4b.herokuapp.com/login"><img id="mapbrew-website-image" src="Assets/Images/mapbrewexample.png" alt="Link to Mapbrew Webpage"></a>
-        <div class="centered">Mapbrew Webpage</div>
+          <a href="https://mapbrew-websitee-20a575919a4b.herokuapp.com/login"><img id="mapbrew-website-image" src={mapbrew} alt="Link to Mapbrew Webpage"/></a>
+        <div className="centered">Mapbrew Webpage</div>
         </div>
         <div id="horiseon-website">
-          <a href="https://nikkivno.github.io/BootcampChallenge1/#social-media-marketing"><img id="horiseon-website-image" src="Assets/Images/websiteexample.png" alt="Link to Horiseon Webpage"></a>
-          <div class="centered">Horiseon Webpage</div>
+          <a href="https://nikkivno.github.io/BootcampChallenge1/#social-media-marketing"><img id="horiseon-website-image" src={horiseon} alt="Link to Horiseon Webpage"/></a>
+          <div className="centered">Horiseon Webpage</div>
           </div>
-          </div>  
-        </div>
-        </div>      */}
-    // </article>
+          </div>   
+    </article>
     );
 }
 
