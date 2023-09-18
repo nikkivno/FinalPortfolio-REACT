823749ded5ad9392c563f717dacc6d13ca9fde23
nikkivno <nikkivigneault@gmail.com>
2023-09-18 20:47:55 -0300

    added in links for icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,10 +9,23 @@ import { faSquareInstagram } from '@fortawesome/free-brands-svg-icons';
 function Footer() {
     return (
         <footer>
-            <div>
-            <FontAwesomeIcon icon={faSquareGitlab} size="2xl" style={{color: "#36454f",}} />
-            <FontAwesomeIcon icon={faLinkedin} size="2xl" style={{color: "#35454f",}} />
-            <FontAwesomeIcon icon={faSquareInstagram} size="2xl" style={{color: "#36454f",}} />
+            <div className="icons">
+                <div id="github">
+                    <a href="https://github.com/nikkivno" target="_blank">
+                        <FontAwesomeIcon icon={faSquareGitlab} size="2xl" style={{color: "#36454f",}} />
+                    </a>
+                </div>
+                <div id="linkedIn">
+                    <a href="https://www.linkedin.com/in/nikki-vigneault-69a054279/" target="_blank">
+                        <FontAwesomeIcon icon={faLinkedin} size="2xl" style={{color: "#35454f",}} />    
+                    </a>
+                </div>
+                <div id="instagram">
+                    <a href="https://www.instagram.com/nikki.v.reads/" target="_blank">
+                        <FontAwesomeIcon icon={faSquareInstagram} size="2xl" style={{color: "#36454f",}} />
+                    </a>
+                </div>
+       
             </div>
             <div>
             <p>This website is property of Nikki Vigneault</p>
