28e9899c91bf60b9cc814ee2cf56e451d8a69eca
nikkivno <nikkivigneault@gmail.com>
2023-09-18 12:17:02 -0300

    added in a header with name

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Header.css';
+
+function Header() {
+    return (
+        <div>
+            <h1>Nikki <span>Vigneault</span></h1>
+        </div>
+    );
+}
+
+export default Header; 
