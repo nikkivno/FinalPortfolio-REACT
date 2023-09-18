fe4e4ebd43db18fae109451a1a13ec35fa7a3d76
nikkivno <nikkivigneault@gmail.com>
2023-09-18 20:29:39 -0300

    added in icons for social media links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import '../styles/Footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquareGitlab } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faSquareInstagram } from '@fortawesome/free-brands-svg-icons';
 
 
 function Footer() {
     return (
         <footer>
+            <div>
+            <FontAwesomeIcon icon={faSquareGitlab} size="2xl" style={{color: "#36454f",}} />
+            <FontAwesomeIcon icon={faLinkedin} size="2xl" style={{color: "#35454f",}} />
+            <FontAwesomeIcon icon={faSquareInstagram} size="2xl" style={{color: "#36454f",}} />
+            </div>
+            <div>
             <p>This website is property of Nikki Vigneault</p>
+            </div>
         </footer>
     );
 }
 
 export default Footer; 
 
-// FontAwesome Icons
-// Gitlab
-{/* <FontAwesomeIcon icon={faSquareGitlab} size="sm" style={{color: "#36454f",}} /> */}
-
-// LinkedIn
-{/* <FontAwesomeIcon icon={faLinkedin} size="sm" style={{color: "#35454f",}} /> */}
