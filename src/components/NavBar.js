5703bb99c250329ace18421a07eccfeaabbe03a3
nikkivno <nikkivigneault@gmail.com>
2023-09-18 19:46:54 -0300

    added in resume title

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,12 +2,14 @@ import React from 'react';
 import '../styles/Navbar.css'
 
 function Navbar() {
+    const currentPage = window.location.pathname;
     return (
     <nav>
       <ul>
-        <li><a href="/">About Me</a></li>
-        <li><a href="/portfolio">Portfolio</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <li><a href="/" className={currentPage === '/' ? 'active' : ''}>About Me</a></li>
+        <li><a href="/portfolio" className={currentPage === '/portfolio' ? 'active' : ''}>Portfolio</a></li>
+        <li><a href="#">Resume</a></li>
+        <li><a href="/contact" className={currentPage === '/contact' ? 'active' : ''}>Contact</a></li>
       </ul>
     </nav>
     );
