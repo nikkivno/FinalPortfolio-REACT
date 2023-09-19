0637571de4963ce13968481ed7ef2160bcce4ac5
nikkivno <nikkivigneault@gmail.com>
2023-09-18 23:50:30 -0300

    added in a possibly template of the project list

---
+++ src/components/Project.js
@@ -0,0 +1,38 @@
+const project = [
+      {
+        title: 'Project 1',
+        imageSrc: 'project1.jpg',
+        websiteUrl: 'https://www.example.com/project1',
+        githubUrl: 'https://github.com/yourusername/project1',
+      },
+      {
+        title: 'Project 1',
+        imageSrc: 'project1.jpg',
+        websiteUrl: 'https://www.example.com/project1',
+        githubUrl: 'https://github.com/yourusername/project1',
+      },
+      {
+        title: 'Project 1',
+        imageSrc: 'project1.jpg',
+        websiteUrl: 'https://www.example.com/project1',
+        githubUrl: 'https://github.com/yourusername/project1',
+      },
+      {
+        title: 'Project 1',
+        imageSrc: 'project1.jpg',
+        websiteUrl: 'https://www.example.com/project1',
+        githubUrl: 'https://github.com/yourusername/project1',
+      },
+      {
+        title: 'Project 1',
+        imageSrc: 'project1.jpg',
+        websiteUrl: 'https://www.example.com/project1',
+        githubUrl: 'https://github.com/yourusername/project1',
+      },
+      {
+        title: 'Project 1',
+        imageSrc: 'project1.jpg',
+        websiteUrl: 'https://www.example.com/project1',
+        githubUrl: 'https://github.com/yourusername/project1',
+      },
+];
