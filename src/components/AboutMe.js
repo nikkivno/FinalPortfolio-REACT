eb58dd60bc6ac32f952e38d68216437e0356d713
nikkivno <nikkivigneault@gmail.com>
2023-09-19 11:44:55 -0300

    about me blurb elongated

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,7 +7,7 @@ function AboutMe() {
     <article class="flexboxcontainer1">
       <div id="aboutme">
           <h2>About Me</h2>
-          <p>Nikki is an aspiring full stack developer with a background in theatre design and production. This history has given her an interest in builidng websites and applications focused on enhancing arts workers experiences through technology.</p>
+          <p>Nikki is an aspiring full stack developer with a background in theatre design and production. This history has given her an interest in builidng websites and applications focused on enhancing arts workers experiences through technology.<br></br>Along with a background in the performing arts, Nikki has a podcast that she founded and cohosts called Bring Your Own Book wherein each episode a new book is explored or an author is interviewed.</p>
       </div>
       <div id="headshot-container" >
       <img id="headshot-image" src={headshot} alt="Nikki's Headshot"/>
