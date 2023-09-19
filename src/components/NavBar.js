45248dbfe3d4d01a20c8a31726b1f8efa136eb33
nikkivno <nikkivigneault@gmail.com>
2023-09-18 23:49:28 -0300

    put in resume placeholder link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ function Navbar() {
       <ul>
         <li><a href="/" className={currentPage === '/' ? 'active' : ''}>About Me</a></li>
         <li><a href="/portfolio" className={currentPage === '/portfolio' ? 'active' : ''}>Portfolio</a></li>
-        <li><a href="#">Resume</a></li>
+        <li><a href="https://drive.google.com/file/d/1POl4R78D3k5BsxL3WoNkdB2zGvwzh6Lm/view?usp=sharing" target="_blank">Resume</a></li>
         <li><a href="/contact" className={currentPage === '/contact' ? 'active' : ''}>Contact</a></li>
       </ul>
     </nav>
