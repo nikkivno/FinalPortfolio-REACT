f29e165d0e393529a5d19375ff93eba85d87cd86
nikkivno <nikkivigneault@gmail.com>
2023-09-19 14:09:56 -0300

    deleted commented out code

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,10 +5,6 @@ import './Project';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSquareGithub } from '@fortawesome/free-brands-svg-icons';
 
-// import mapbrew from '../images/mapbrewexample.png';
-// import budgetsense from '../images/budgetexample.png';
-// import horiseon from '../images/websiteexample.png';
-
 
 
 const Portfolio = ({title, imageSrc, githubUrl, websiteUrl}) => {
@@ -30,30 +26,6 @@ const Portfolio = ({title, imageSrc, githubUrl, websiteUrl}) => {
 
   );
 };
-
-// function Portfolio() {
-//     return (
-//         <article className="flexboxcontainer2" id="portfolio">
-//         <div className="flexboxcontainer4">
-//           <h2>My Work</h2>
-//         </div>
-//         <div className="flexboxcontainer6">
-//           <div id="budget-website">
-//             <a href="https://nikkivno.github.io/BudgetingWebsite-BootcampProject1/"><img id="budget-website-image" src={budgetsense} alt="Link to BudgetSense Website"/></a>
-//           <div className="centered"> BudgetSense Webpage</div>
-//         </div>  
-//         <div id="mapbrew-website">
-//           <a href="https://mapbrew-websitee-20a575919a4b.herokuapp.com/login"><img id="mapbrew-website-image" src={mapbrew} alt="Link to Mapbrew Webpage"/></a>
-//         <div className="centered">Mapbrew Webpage</div>
-//         </div>
-//         <div id="horiseon-website">
-//           <a href="https://nikkivno.github.io/BootcampChallenge1/#social-media-marketing"><img id="horiseon-website-image" src={horiseon} alt="Link to Horiseon Webpage"/></a>
-//           <div className="centered">Horiseon Webpage</div>
-//           </div>
-//           </div>   
-//     </article>
-//     );
-// }
 
 export default Portfolio;
 
