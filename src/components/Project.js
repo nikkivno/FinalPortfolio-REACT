32b5acf224896d4f35dc248f8d6abf9df74188f4
nikkivno <nikkivigneault@gmail.com>
2023-09-19 14:06:23 -0300

    edits made to project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,38 +1,63 @@
-const project = [
+import React from 'react';
+import Portfolio from './Portfolio';
+import'../styles/Portfolio.css';
+
+
+const Project = () => {
+  const projects = [
       {
-        title: 'Project 1',
-        imageSrc: 'project1.jpg',
-        websiteUrl: 'https://www.example.com/project1',
-        githubUrl: 'https://github.com/yourusername/project1',
+        title: 'Mapbrew',
+        imageSrc: './images/mpabrewexample.png',
+        websiteUrl: 'https://mapbrew-websitee-20a575919a4b.herokuapp.com/login',
+        githubUrl: 'https://github.com/YggdrasilJL/mapbrew-haven',
       },
-      {
-        title: 'Project 1',
-        imageSrc: 'project1.jpg',
-        websiteUrl: 'https://www.example.com/project1',
-        githubUrl: 'https://github.com/yourusername/project1',
-      },
-      {
-        title: 'Project 1',
-        imageSrc: 'project1.jpg',
-        websiteUrl: 'https://www.example.com/project1',
-        githubUrl: 'https://github.com/yourusername/project1',
-      },
-      {
-        title: 'Project 1',
-        imageSrc: 'project1.jpg',
-        websiteUrl: 'https://www.example.com/project1',
-        githubUrl: 'https://github.com/yourusername/project1',
-      },
-      {
-        title: 'Project 1',
-        imageSrc: 'project1.jpg',
-        websiteUrl: 'https://www.example.com/project1',
-        githubUrl: 'https://github.com/yourusername/project1',
-      },
-      {
-        title: 'Project 1',
-        imageSrc: 'project1.jpg',
-        websiteUrl: 'https://www.example.com/project1',
-        githubUrl: 'https://github.com/yourusername/project1',
-      },
+      // {
+      //   title: 'Project 1',
+      //   imageSrc: 'project1.jpg',
+      //   websiteUrl: 'https://www.example.com/project1',
+      //   githubUrl: 'https://github.com/yourusername/project1',
+      // },
+      // {
+      //   title: 'Project 1',
+      //   imageSrc: 'project1.jpg',
+      //   websiteUrl: 'https://www.example.com/project1',
+      //   githubUrl: 'https://github.com/yourusername/project1',
+      // },
+      // {
+      //   title: 'Project 1',
+      //   imageSrc: 'project1.jpg',
+      //   websiteUrl: 'https://www.example.com/project1',
+      //   githubUrl: 'https://github.com/yourusername/project1',
+      // },
+      // {
+      //   title: 'Project 1',
+      //   imageSrc: 'project1.jpg',
+      //   websiteUrl: 'https://www.example.com/project1',
+      //   githubUrl: 'https://github.com/yourusername/project1',
+      // },
+      // {
+      //   title: 'Project 1',
+      //   imageSrc: 'project1.jpg',
+      //   websiteUrl: 'https://www.example.com/project1',
+      //   githubUrl: 'https://github.com/yourusername/project1',
+      // },
 ];
+
+return (
+  <div>
+    {projects.map((project, index) => (
+      <Portfolio
+      key={index}
+      title={project.title}
+      imageSrc={project.imageSrc}
+      githubUrl={project.githubUrl}
+      websiteUrl={project.websiteUrl}
+      />
+    ))};
+  </div>
+);
+
+};
+
+export default Project;
+
