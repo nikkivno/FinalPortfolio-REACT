5b4e95458878f34dc23e03b2b1f3dd19116be04d
nikkivno <nikkivigneault@gmail.com>
2023-09-19 20:18:46 -0300

    changed some left over class titles to className

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,7 +4,7 @@ import headshot from '../images/avatar.jpg';
 
 function AboutMe() {
     return (
-    <article class="flexboxcontainer1">
+    <article className="flexboxcontainer1">
       <div id="aboutme">
           <h2>About Me</h2>
           <p>Nikki is an aspiring full stack developer with a background in theatre design and production. This history has given her an interest in builidng websites and applications focused on enhancing arts workers experiences through technology.<br></br>Along with a background in the performing arts, Nikki has a podcast that she founded and cohosts called Bring Your Own Book wherein each episode a new book is explored or an author is interviewed.</p>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -10,17 +10,19 @@ import { faSquareGithub } from '@fortawesome/free-brands-svg-icons';
 const Portfolio = ({title, imageSrc, githubUrl, websiteUrl}) => {
   console.log(title, imageSrc, githubUrl, websiteUrl);
   return(
+    <div>
     <div className='container'>
-      <a href={websiteUrl} target='_blank' class='website'><img src={imageSrc} id='image'/></a>
-    <div class="overlay">
+      <a href={websiteUrl} target='_blank' className='website'><img src={imageSrc} id='image'/></a>
+    <div className="overlay">
       <div className='hidden-text'>
         <h1>{title}</h1>
       </div>
 
       <div className='hidden-text'>
-        <a href={githubUrl} target="_blank" class="github"><FontAwesomeIcon icon={faSquareGithub} size="2xl" style={{color: "#36454f",}}/></a>
+        <a href={githubUrl} target="_blank" className="github"><FontAwesomeIcon icon={faSquareGithub} size="2xl" style={{color: "#36454f",}}/></a>
       </div>    
       </div>
+    </div>
     </div>
 
   );
