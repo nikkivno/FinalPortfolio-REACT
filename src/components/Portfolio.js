3a92b9724c3dfa13f006e389b95674b42eccbc48
nikkivno <nikkivigneault@gmail.com>
2024-01-23 00:00:42 -0400

    netlify bug fixes, alt for images, target noreferrer, etc

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -12,13 +12,13 @@ const Portfolio = ({title, imageSrc, githubUrl, websiteUrl}) => {
   return(
     <div>
     <div className='container'>
-      <a href={websiteUrl} target='_blank' className='website'><img src={imageSrc} id='image'/></a>
+      <a href={websiteUrl} target='_blank' rel="noreferrer" className='website'><img src={imageSrc} id='image' alt='Portfolio Card with a link to a sample of my work' /></a>
     <div className="overlay">
       <div className='hidden-text'>
         <h1>{title}</h1>
       </div>
       <div className='hidden-text'>
-        <a href={githubUrl} target="_blank" className="github"><FontAwesomeIcon icon={faSquareGithub} size="2xl" style={{color: "#36454f",}}/></a>
+        <a href={githubUrl} target="_blank" rel="noreferrer" className="github"><FontAwesomeIcon icon={faSquareGithub} size="2xl" style={{color: "#36454f",}}/></a>
       </div>    
       </div>
     </div>
