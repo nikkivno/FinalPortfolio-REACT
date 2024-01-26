472ee39a3a03acaf28ed848417587a4edf9fd958
nikkivno <nikkivigneault@gmail.com>
2024-01-25 20:14:37 -0400

    updated about me section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,7 +7,7 @@ function AboutMe() {
     <article className="flexboxcontainer1">
       <div id="aboutme">
           <h2>About Me</h2>
-          <p>Nikki is an aspiring front-end developer with a background in theatre design and production. This history has given her an interest in building websites and applications focused on enhancing arts workers experiences through technology.<br></br>Along with a background in the performing arts, Nikki has a podcast that she founded and cohosts called Bring Your Own Book wherein each episode a new book is explored or an author is interviewed.</p>
+          <p>Nikki is an aspiring front-end developer with a background in theatre design and production management. This history has given her an interest in building websites and applications focused on enhancing arts workers experiences through technology.<br></br>Along with a background in the performing arts, Nikki has a podcast that she founded and cohosts called Bring Your Own Book wherein each episode a new book is explored or an author is interviewed.</p>
       </div>
       <div id="headshot-container" >
       <img id="headshot-image" src={headshot} alt="Nikki's Headshot"/>
