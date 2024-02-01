e1880267b20814df5b3492f3889873b0b7c246a3
nikkivno <nikkivigneault@gmail.com>
2024-02-01 03:28:20 -0400

    updated portfolio

--- src/Data.js
+++ src/Data.js
@@ -5,10 +5,17 @@ import weatherapp from './images/WeatherApp.png';
 import fantasyquiz from './images/Fantasy Quiz.png';
 import notetaker from './images/noteTaker.png';
 import writersroom from './images/writersroom.png';
+import d3samples from './images/D3.js Example Site.png';
 
 
 
 export const projects = [
+    {
+        title: 'Click to View Work',
+        imageSrc: d3samples,
+        websiteUrl: 'https://nikkivno.github.io/d3.jsSamples/',
+        githubUrl: 'https://github.com/nikkivno/d3.jsSamples',
+    },
     {
         title: 'Click to View Work',
         imageSrc: writersroom,
@@ -20,12 +27,6 @@ export const projects = [
         imageSrc: mapbrew,
         websiteUrl: 'https://mapbrew-websitee-20a575919a4b.herokuapp.com/login',         
         githubUrl: 'https://github.com/YggdrasilJL/mapbrew-haven',
-    },
-    {
-        title: 'Click to View Work',
-        imageSrc: budgetsense,
-        websiteUrl:'https://nikkivno.github.io/BudgetingWebsite-BootcampProject1/',
-        githubUrl: 'https://github.com/nikkivno/BudgetingWebsite-BootcampProject1',
     },
     {
         title: 'Click to View Work',
